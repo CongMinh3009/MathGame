c547a82da9db42711caebb9dc390291b0ade6e6f
CongMinh3009 <renbi3009@gmail.com>
2023-04-27 15:28:48 +0700

    update type Question and add slider demo

--- _myProject/Scripts/GameContrl.cs
+++ _myProject/Scripts/GameContrl.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -37,12 +37,14 @@ public class GameContrl : MonoBehaviour
 
            if(oper == 3)
             {
+            // xử lý nếu random oper là chia 
                 int temp = UnityEngine.Random.Range(1, 5);
                 number2 = UnityEngine.Random.Range(1,10);
                 number1 = temp * number2;
             }
            else if(oper == 1)
             {
+            // xử lý nếu random oper là trừ
                 number2 = UnityEngine.Random.Range(1, 10);
                 number1 = UnityEngine.Random.Range(1 + number2 / 2, 10);
             }
@@ -56,17 +58,51 @@ public class GameContrl : MonoBehaviour
     }
     void SetText(int n1 ,int  n2 ,int  oper )
     {
-        int relust;
-        QuestionNumber1.text = n1.ToString();
-        QuestionNumber2.text = n2.ToString();
+        int TYPE_QUESTION = UnityEngine.Random.Range(0, 4);
+        int relust = RelustAnswer(n1,n2,oper);
+        // tạo chấm hỏi để tạo các loại câu hỏi cho user trả lời
+        if(TYPE_QUESTION == 0)
+        {
+            QuestionNumber1.text = "?";
+            QuestionNumber2.text = n2.ToString();
+            Operator.text = GetOperator(oper);
+            Relust.text = relust.ToString();
+        }
+        else if(TYPE_QUESTION == 1)
+        {
+            QuestionNumber1.text = n1.ToString();
+            QuestionNumber2.text = "?";
+            Operator.text = GetOperator(oper);
+            Relust.text = relust.ToString();
+        }
+        else if(TYPE_QUESTION == 2)
+        {
+            QuestionNumber1.text = n1.ToString();
+            QuestionNumber2.text = n2.ToString();
+            Operator.text = GetOperator(oper);
+            Relust.text = "?";
+        }
+        else if(TYPE_QUESTION == 3)
+        {
+            QuestionNumber1.text = n1.ToString();
+            QuestionNumber2.text = n2.ToString();
+            Operator.text = "?";
+            Relust.text = relust.ToString();
+        }
+        //active animation cho ô bị châm hỏi
+        QuestionNumber1.transform.parent.Find("Selected").gameObject.SetActive(TYPE_QUESTION == 0);
+        QuestionNumber2.transform.parent.Find("Selected").gameObject.SetActive(TYPE_QUESTION == 1);
+        Operator.transform.parent.Find("Selected").gameObject.SetActive(TYPE_QUESTION == 3);
+        Relust.transform.parent.Find("Selected").gameObject.SetActive(TYPE_QUESTION == 2);
 
-        if (oper == 0) Operator.text = "+";
-        else if (oper == 1) Operator.text = "-";
-        else if (oper == 2) Operator.text = "*";
-        else Operator.text = "/";
-
-        relust = RelustAnswer(n1, n2, oper);
-        Relust.text = relust.ToString();
+    }
+    public string GetOperator(int oper)
+    {
+        if (oper == 0) return "+";
+        else if (oper == 1) return "-";
+        else if (oper == 2) return "*";
+        else if (oper == 3) return "/";
+        else return null;
     }
     public int RelustAnswer(int n1, int n2, int oper)
     {
